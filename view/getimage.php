7a826ab31642172c547b80fa411cc0917bfaa3e8
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2014-05-15 04:13:29 +0200

    Shrinking speedups

--- view/getimage.php
+++ view/getimage.php
@@ -31,12 +31,18 @@
 			case 'smallthumb':$width=170;$height=170;$minimum='minimum';$download=false;break;
 			case 'thumb':$width=250;$height=250;$minimum='minimum';$download=false;break;
 			case 'preview':$width=450;$height=338;$minimum='';$download=false;break;
-			default:$width=1000000;$height=1080;$minimum='';$download=false;break;
+			default:
+				$width=1000000;$height=1080;$minimum='';$download=false;
+			break;
 		}
 	}
 	
 	if (!$userIsAdmin && $width==0 &&!$download){
 		$width=10000000; $height=10000000;
+	}
+	
+	if (!$user && $download){
+		$width=2048;$height=2048;
 	}
 	
 	$isLimited=($width>0);
@@ -183,11 +189,11 @@ function shrinkImage($lokalurl,$limitWidth,$limitHeight,$rotate,$cachePath,$key,
 	 //Shrinking of the image
 	 if ($minimum){
 	 	$temp=ImageCreateTrueColor($limitWidth,$limitHeight);
-	 	ImageCopyResampled($temp,$original,-($newwidth-$limitWidth)/2,-($newheight-$limitHeight)/4,0,0,$newwidth,$newheight,$origwidth,$origheight);
+	 	fastimagecopyresampled($temp,$original,-($newwidth-$limitWidth)/2,-($newheight-$limitHeight)/4,0,0,$newwidth,$newheight,$origwidth,$origheight);
 	 }
 	 else {
 	   $temp=ImageCreateTrueColor($newwidth,$newheight);
-	   ImageCopyResampled($temp,$original,0,0,0,0,$newwidth,$newheight,$origwidth,$origheight);
+	   fastimagecopyresampled($temp,$original,0,0,0,0,$newwidth,$newheight,$origwidth,$origheight);
 	 } 
 	 
 	 if ($rotate=='rotate'){$temp=ImageRotateRightAngle ($temp, 180);}
@@ -320,5 +326,29 @@ function ImageRotateRightAngle( $imgSrc, $angle )
 
     return( $imgDest ); 
 } 
+
+function fastimagecopyresampled (&$dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h, $quality = 3) {
+  // Plug-and-Play fastimagecopyresampled function replaces much slower imagecopyresampled.
+  // Just include this function and change all "imagecopyresampled" references to "fastimagecopyresampled".
+  // Typically from 30 to 60 times faster when reducing high resolution images down to thumbnail size using the default quality setting.
+  // Author: Tim Eckel - Date: 09/07/07 - Version: 1.1 - Project: FreeRingers.net - Freely distributable - These comments must remain.
+  //
+  // Optional "quality" parameter (defaults is 3). Fractional values are allowed, for example 1.5. Must be greater than zero.
+  // Between 0 and 1 = Fast, but mosaic results, closer to 0 increases the mosaic effect.
+  // 1 = Up to 350 times faster. Poor results, looks very similar to imagecopyresized.
+  // 2 = Up to 95 times faster.  Images appear a little sharp, some prefer this over a quality of 3.
+  // 3 = Up to 60 times faster.  Will give high quality smooth results very close to imagecopyresampled, just faster.
+  // 4 = Up to 25 times faster.  Almost identical to imagecopyresampled for most images.
+  // 5 = No speedup. Just uses imagecopyresampled, no advantage over imagecopyresampled.
+
+  if (empty($src_image) || empty($dst_image) || $quality <= 0) { return false; }
+  if ($quality < 5 && (($dst_w * $quality) < $src_w || ($dst_h * $quality) < $src_h)) {
+    $temp = imagecreatetruecolor ($dst_w * $quality + 1, $dst_h * $quality + 1);
+    imagecopyresized ($temp, $src_image, 0, 0, $src_x, $src_y, $dst_w * $quality + 1, $dst_h * $quality + 1, $src_w, $src_h);
+    imagecopyresampled ($dst_image, $temp, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $dst_w * $quality, $dst_h * $quality);
+    imagedestroy ($temp);
+  } else imagecopyresampled ($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+  return true;
+}
 
 ?>
